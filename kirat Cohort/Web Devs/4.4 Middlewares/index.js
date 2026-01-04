de7b4963a5c4ede5ad9a40ae69d1822f730a6041
vijendra8475 <vijjubca@gmail.com>
2026-01-04 23:55:15 +0530

    4.4 Middleware [ Structure created ]

---
+++ kirat Cohort/Web Devs/4.4 Middlewares/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+})
+
+app.post('/', (req, res) => {
+
+})
+
+app.put('/', (req, res) => {
+
+})
+
+app.delete('/', (req, res) => {
+
+})
+
+
+
+app.listen(3000);
