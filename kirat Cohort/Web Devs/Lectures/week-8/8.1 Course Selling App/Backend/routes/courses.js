e9edf81187d1baa088630b9e6588f287f1e114aa
vijendra8475 <vijjubca@gmail.com>
2026-01-14 23:46:31 +0530

    week-8  [ Some course routes added ]

--- kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/courses.js
+++ kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/courses.js
@@ -2,9 +2,15 @@ const { Router } = require('express')
 const router = Router();
 
 
-router.get('/', (req, res) => {
+router.get('/purchase', (req, res) => {
     res.json({
         message : "getting all courses"
+    })
+})
+
+router.get('/preview', (req, res) => {
+    res.json({
+        message : "preview a course"
     })
 })
 
