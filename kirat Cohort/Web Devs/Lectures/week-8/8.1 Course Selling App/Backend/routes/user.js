1c7a83e29fb3bbbb4cc910272ce432dc4fe9729f
vijendra8475 <vijjubca@gmail.com>
2026-01-15 22:32:09 +0530

    week-8 [ User signup route created ]

--- kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/user.js
+++ kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/user.js
@@ -1,10 +1,30 @@
 const { Router } = require('express')
+const { userModel } = require('../DB/models')
+const bcrypt = require('bcrypt')
 const router = Router();
 
-router.post('/signup', (req, res) => {
-    res.json({
-        message : 'signup route'
-    })
+router.post('/signup', async (req, res) => {
+
+    const { email, password, firstName, lastName } = req.body;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        userModel.create({
+            email,
+            password : hashedPassword,
+            firstName,
+            lastName
+        })
+
+        res.json({
+            message : 'signup success'
+        })
+    }
+    catch(e) {
+        res.json({
+            message : 'Something went wrong at signup route'
+        })
+    }
 })
 
 router.post('/signin', (req, res) => {
