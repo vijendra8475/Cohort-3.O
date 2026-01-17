8e852e36dd839b801d42ce87ca22a7c07ec2c473
vijendra <vijjubca@.com>
2026-01-17 23:46:51 +0530

    week-8 [ Admin signup & signin routes Completed ]

--- kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/admin.js
+++ kirat Cohort/Web Devs/Lectures/week-8/8.1 Course Selling App/Backend/routes/admin.js
@@ -1,15 +1,54 @@
-const { Router } = require('express')
+const { Router } = require('express');
+const { adminModel } = require('../DB/models');
+const bcrypt = require('bcrypt')
 const router = Router();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
-router.post('/signup', (req, res) => {
-    res.json({
-        message : 'signup route'
-    })
+dotenv.config();
+
+router.post('/signup', async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
+    
+        try{
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await adminModel.create({
+                email,
+                password : hashedPassword,
+                firstName,
+                lastName
+            })
+    
+            res.json({
+                message : 'Admin signup success'
+            })
+        }
+        catch(e) {
+            res.json({
+                message : 'Something went wrong at signup route',
+            })
+        }
 })
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await adminModel.findOne({ email })
+
+    if(!user)
+        return res.status(403).json({ message : 'Invalid credentials'})
+
+    const isPasswordCorrect = bcrypt.compare(password, user.password);
+
+    if(!isPasswordCorrect)
+        return res.status(403).json({ message : 'Invalid Credentials'})
+
+    const token = jwt.sign({ id : user._id }, process.env.JWT_SECRET_FOR_ADMIN);
+
+
     res.json({
-        message : 'signin route'
+        message : 'signin route',
+        token
     })
 })
 
